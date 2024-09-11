9fb9100204e23e714df56ac2bcdc0f7d057fd42a
krzysztof <larkfreeme70.55@gmail.com>
2024-09-11 08:23:25 +0200

    feat(tests): add basic jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: ["/node_modules/@react-native-community/async-storage/(?!(lib))"],
+  coveragePathIgnorePatterns: ['index.ts', 'src/api/', 'src/shared/assets/'],
+  testMatch: [' **/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 };
